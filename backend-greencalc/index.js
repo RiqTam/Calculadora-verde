7342500415a32d751acf1b40def26f678dcc36cd
Abdiel Reyes <abykings1@hotmail.com>
2021-10-22 14:17:34 -0500

    updated something to see server changes

--- backend-greencalc/index.js
+++ backend-greencalc/index.js
@@ -17,7 +17,7 @@ app.use('/api/user',authRoute)
 app.use('/api/posts',postRoute)
 
 app.get('/',(req,res)=>{
-	res.send("Greetings from Dinohacks team!")
+	res.send(JSON.parse("{message:'Hi from Dinohack team! '}"))
 })
 
 
