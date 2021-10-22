0699cb5ae9dfe84c0f1c09ab0153ccaf852ca4a0
Abdiel Reyes <abykings1@hotmail.com>
2021-10-22 13:27:00 -0500

    added new db and updated port

--- backend-greencalc/index.js
+++ backend-greencalc/index.js
@@ -16,4 +16,4 @@ app.use(express.json())
 app.use('/api/user',authRoute)
 app.use('/api/posts',postRoute)
 
-app.listen(3000,()=> console.log('Server up.'))
+app.listen(8080,()=> console.log('Server up.'))
