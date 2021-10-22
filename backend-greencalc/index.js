a8efb537594a6d95c641c1d178fa723925f468b4
Abdiel Reyes <abykings1@hotmail.com>
2021-10-22 14:45:30 -0500

    updated something to see server changes

--- backend-greencalc/index.js
+++ backend-greencalc/index.js
@@ -5,12 +5,9 @@ const dotenv = require('dotenv');
 const authRoute = require('./routes/auth');
 const postRoute = require('./routes/posts')
     
-
-
 dotenv.config()
 
 mongoose.connect(process.env.DB_CONNECT,()=>{ console.log("Connected to db") })
-
 
 app.use(express.json())
 app.use('/api/user',authRoute)
