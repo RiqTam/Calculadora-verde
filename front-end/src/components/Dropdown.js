5795b4a84d94ccffa288e1f5b249f60d7f3cd9e6
SaguiraBlack <josefina10969@gmaill.com>
2021-10-23 15:24:35 -0500

    Added json for solutions

--- front-end/src/components/Dropdown.js
+++ front-end/src/components/Dropdown.js
@@ -3,11 +3,11 @@ import React, { useState } from 'react'
 import Label from './Label'
 
 export default function Dropdown(props) {
-    const [down, goDown] = useState(true)
+    const [down, goDown] = useState(false)
     return (
         <div className={props.className+" select-none my-8"}>
             
-                <div onClick={()=> goDown(!down)} className="border-4 border-green rounded-md w-3/5 p-4">
+                <div onClick={()=> goDown(!down)} className="border-4 border-green rounded-md w- p-4">
                     <div>
                         <Label label={props.label} className="font-medium text-xl"/>
                         <p className="font-light">
