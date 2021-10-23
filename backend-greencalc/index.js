e0c615493e71abe59deae93c830c7299a675e0c8
Abdiel Reyes <abykings1@hotmail.com>
2021-10-23 00:35:17 -0500

    added cors

--- backend-greencalc/index.js
+++ backend-greencalc/index.js
@@ -5,6 +5,9 @@ const dotenv = require('dotenv');
 const authRoute = require('./routes/auth');
 const postRoute = require('./routes/posts')
 const bimesterRoute = require('./routes/bimester/bimester')
+const cors = require('cors')
+
+
 
 dotenv.config()
 
@@ -17,6 +20,7 @@ mongoose.connect(process.env.DB_CONNECT,(err)=>{
 })
 
 app.use(express.json())
+app.use(cors());
 
 app.use('/api/user',authRoute)
 app.use('/api/post',postRoute)
