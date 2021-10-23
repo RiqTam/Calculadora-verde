f5d989c1761a0f83553b0770f0a366869118509c
AlanPL <apyolt@gmail.com>
2021-10-23 15:50:45 -0500

    show co2_emition

--- front-end/src/pages/Fingerprint.js
+++ front-end/src/pages/Fingerprint.js
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useState } from 'react';
 import Button from '../components/Button'
 import CircleGraph from '../components/CircleGraph';
 import Title from '../components/Title'
+import useAuth from '../hooks/useAuth';
 import happyDino from '../images/happyDino.png';
 
 export default function Fingerprint() {
+	const auth = useAuth();
+	const [bimesters, setBimesters] = useState([]);
+
+	useEffect(() => {
+		auth.request("bimester/getMyBimester","POST")
+		.then(res => {
+			console.log(res.data);
+			setBimesters(res.data);
+		})
+        .catch((error) =>{
+			console.log(error);
+        });
+
+	}, [auth])
+
+	if(bimesters.length<1) return '';
 	return (
 		<div className='p-32 bg-white-dark'>
 			<Title title={"Mi Huella"} /><br/>
@@ -15,7 +33,7 @@ export default function Fingerprint() {
 				<div className='flex w-2/3 '>
 					<div className="flex-col ">
 						<p className="font-semibold text-4xl">
-							Tu huella actual es <br/>XXX
+							Tu huella actual es <br/>{bimesters[bimesters.length-1].co2_emitido}
 						</p>
 						<div className="m-auto px-20 pt-0 flex">
 							<CircleGraph  from={"#014A00"} to={"#7CC72D" } percentage={20} id="actual"/>
@@ -23,7 +41,7 @@ export default function Fingerprint() {
 					</div>
 					<div className="flex-col ">
 						<p className="text-lg">
-							Huella anterior<br/> XXX
+							Huella anterior<br/> {bimesters[bimesters.length-2].co2_emitido}
 						</p>
 						<div className=" m-auto px-32 pt-0 flex">
 							<CircleGraph  from={"#710808"} to={"#F12C2C" } percentage={80} id="before"/>
