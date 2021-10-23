4ca184c3497e244d403440555dd55d9e67d3b1c1
AlanPL <apyolt@gmail.com>
2021-10-22 22:26:28 -0500

    add responsive design for login

--- front-end/src/components/ButtonHeader.js
+++ front-end/src/components/ButtonHeader.js
@@ -4,7 +4,8 @@ export default function ButtonHeader({label, onClick}) {
     return (
         <div>
             <button className="bg-white bg-opacity-30 rounded-full border-4 border-white
-                                py-3 px-8 font-bold text-white text-opacity-100" onClick={onClick}>
+                                py-3 px-8 font-bold text-white text-opacity-100" 
+                    onClick={onClick}>
                 {label}
             </button>
         </div>
--- front-end/src/components/Title.js
+++ front-end/src/components/Title.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Title({title, textSize, className}) {
 	return (
-		<div className={`${ textSize || 'text-5xl'} font-black ${className}`}>
+		<div className={`${ textSize || 'text-3xl xl:text-5xl'} font-black ${className}`}>
 			{title}
 		</div>
 	)
