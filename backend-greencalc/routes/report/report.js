fd2178646d93f184cd695b735d92489079f59c10
Abdiel Reyes <abykings1@hotmail.com>
2021-10-24 02:21:41 -0500

    added route point

--- backend-greencalc/routes/report/report.js
+++ backend-greencalc/routes/report/report.js
@@ -1,11 +1,17 @@
 const router = require('express').Router();
 const Bimester = require('../../models/Bimester');
+const User = require('../../models/User')
 const verify = require('../verifyToken')
 
 
-router.post('/',verify, async (req,res)=>{
-	res.status(400).json({message:"I'm in"})
-});
+router.post('/getPoints',verify, async (req,res)=>{
+	let user = await User.findOne({_id:req.body.user_id})
+	if(!user){
+		res.status(400).json({message:'User not found'})
+	}else{
+		res.status(200).json(user.points)
+	}
+})
 
 
 
