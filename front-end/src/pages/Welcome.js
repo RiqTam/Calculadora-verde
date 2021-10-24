174a365a1f8d68156c7e1275b2229c5730d03ff2
AlanPL <apyolt@gmail.com>
2021-10-24 01:07:36 -0500

    world changes

--- front-end/src/pages/Welcome.js
+++ front-end/src/pages/Welcome.js
@@ -14,14 +14,14 @@ export default function Welcome() {
     return (
         <div className="bg-white-dark h-screen flex-col flex">
             <div className="flex flex-col md:flex-row h-full">
-                <section className="m-auto w-full md:w-3/5 p-16 pt-20 pb-0 md:pl-20 xl:p-64 flex-col  h-full">
-                    <video autoPlay loop muted  playsInline  className=" w-full h-auto">
+                <section className="m-auto w-full md:w-3/5 p-16 pt-40 pb-0 md:pl-20 xl:px-60  flex-col  h-full">
+                    <video autoPlay loop muted  playsInline  className=" w-full h-auto rounded-2xl">
                         <source src={world} type="video/mp4"/>
                         Your Browser does not support video tag
                     </video>
 
                 </section>
-                <section className=" flex-col w-full md:w-2/5  pt-40">
+                <section className=" flex-col w-full md:w-2/5  pt-40 px-10">
                         <Title title={`Bienvenido(a), ${auth.user.name}`} className="text-green-dark mb-10" />
                         <Label label="Parece que aún no tenemos información para calcular tu huella..." 
                                 className="md:text-2xl"/>
